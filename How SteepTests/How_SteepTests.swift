0b010982a6f9dc23d18f030afa03b8742ab4064f
Adam Leuer <aleuer@thoughtworks.com>
2021-06-14 21:30:19 -0700

    Initial Commit

---
+++ How SteepTests/How_SteepTests.swift
@@ -0,0 +1,34 @@
+//
+//  How_SteepTests.swift
+//  How SteepTests
+//
+//  Created by Adam James Leuer on 6/14/21.
+//  Copyright © 2021 Adam James Leuer. All rights reserved.
+//
+
+import XCTest
+@testable import How_Steep
+
+class How_SteepTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
